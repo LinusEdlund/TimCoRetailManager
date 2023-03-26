7676f9f6236e9329db23222bb4ba5a90ff59399a
Linus Edlund <Linus.Edlund@hotmail.com>
2023-03-26 16:53:43 +0200

    Add Admin Endpoint Data
    Adds the data for administrative endpoints. Dose not yet secure the endpoints beyond authorization.

---
+++ TMRDataManager.Library/DataAccess/InventoryData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMRDataManager.Library.Internal.DataAccess;
+
+namespace TMRDataManager.Library.DataAccess
+{
+    public class InventoryData
+    {
+        public List<InventoryModel> GetInventory()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<InventoryModel, dynamic>("spInventory_GetAll", new { }, "TRMData");
+
+            return output;
+        }
+
+        public void SaveInventoryRecord(InventoryModel item)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            sql.SaveData("dbo.spInventory_Insert", item, "TRMData");
+
+        }
+    }
+}
---
+++ TMRDataManager.Library/Models/SaleReportModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace TMRDataManager.Library.DataAccess
+{
+    public class SaleReportModel
+    {
+        public DateTime SaleDate { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Tax { get; set; }
+        public decimal Total { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
